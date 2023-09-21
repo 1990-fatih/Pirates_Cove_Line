a721904893f576f4717817ccceafdb5382e70f7b
Fatih Dogan <fatih.dogan@bitlc.onmicrosoft.com>
2023-09-21 15:53:09 +0200

    Projektdateien hinzufügen.

---
+++ Pirates_Cove_Line/Bus.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pirates_Cove_Line
+{
+    internal class Bus
+    {
+        Queue<Golfer> Qgolfers = new Queue<Golfer>();
+        Stack<Golfer> Sgolfer = new Stack<Golfer>();
+        private bool tuer;
+
+        
+
+
+        public void Buseinsteigen(Golfer g)
+        {
+            Qgolfers.Enqueue(g);
+
+            Console.WriteLine(g.Name+" Golfer steigt in Bus");
+        }
+
+      
+        public void GolferAussteigen () 
+        {
+            if (Qgolfers.Count>0)
+            {
+                Console.WriteLine(Qgolfers.Dequeue().Name+" ist ausgesteigt!!");
+                Qgolfers.Dequeue();
+                
+            }
+            else
+            {
+                Sgolfer.Pop();
+                Console.WriteLine(Sgolfer.Pop().Name + " ist ausgesteigt!!");
+
+            }
+        }
+
+        public void ListevonBus()
+        {
+            Console.WriteLine("Liste von bus: ");
+            if (Qgolfers.Count>0)
+            {
+                foreach (var item in Qgolfers)
+                {
+                    Console.WriteLine("Golfers: " + item.Name);
+                }
+            }
+            else
+            {
+                foreach (var item in Sgolfer)
+                {
+                    Console.WriteLine("Golfers: " + item.Name);
+                }
+            }
+            
+        }
+
+        public void türkaputt() 
+        {
+            while (Qgolfers.Count>0)
+            { Sgolfer.Push(Qgolfers.Dequeue()); }
+
+            tuer = false;
+
+
+            //foreach(var item in Qgolfers)
+            //{
+            //    Sgolfer.Push(item.);
+            //}
+            //if (Qgolfers.Count>0)
+            //{
+            //    Qgolfers.Dequeue();
+            //}
+        }
+
+
+    }
+}
---
+++ Pirates_Cove_Line/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates_Cove_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+
+            Golfer n = new Golfer() { Name="Golfer1"};
+            Golfer n2 = new Golfer() { Name="Golfer2" };
+            Golfer n3= new Golfer() { Name="Golfer3" };
+            Golfer n4 = new Golfer() { Name="Golfer4" };
+            Golfer n5 = new Golfer() { Name="Golfer5" };
+            Golfer n6 = new Golfer() { Name="Golfer6" };
+            Golfer n7 = new Golfer() { Name="Golfer7" };
+
+            bus.Buseinsteigen(n);
+            bus.Buseinsteigen(n2);
+            bus.Buseinsteigen(n3);
+            bus.Buseinsteigen(n4);
+            bus.Buseinsteigen(n5);
+            bus.Buseinsteigen(n6);
+            bus.Buseinsteigen(n7);
+
+
+            Console.WriteLine();
+            
+            bus.ListevonBus();
+            Console.WriteLine();
+
+            bus.GolferAussteigen();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Tür kaput");
+
+            bus.türkaputt();
+
+            bus.ListevonBus();
+
+
+        }
+    }
+}
